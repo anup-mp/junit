8cc3f040c242ebd8be2fd0eb528238a327b2fb55
unknown <sontrng@BG1WS0394.SONATA.LOCAL>
2019-03-20 14:40:19 +0530

    done

---
+++ src/com/devops/JunitTestcaseTest1.java
@@ -0,0 +1,23 @@
+package com.devops;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JunitTestcaseTest1 {
+
+	@Test
+	void test() {
+		//CustomerRegister cust=new CustomerRegister();
+		equals("Login1");
+		equals("CustomerName");
+	}
+
+	@Test
+	void test1() {
+		//CustomerRegister cust=new CustomerRegister();
+		equals("Login");
+		equals("CustomerEmail");
+	}
+
+}
